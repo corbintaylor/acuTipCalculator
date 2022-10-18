0632c08ab4088bff93b7f684e40c8b917c2321c8
23CTAYLOR1 <01.corbint@gmail.com>
2022-10-18 09:03:08 -0600

    Progress 10/18

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Do you see this
-        //I do
     }
+
+
 }
